7bd8dd00851a699eec3354a66116e0657e866191
nathalie <nathalie-nordstrom@hotmail.com>
2020-11-17 15:37:54 +0100

    nathalie done driving

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,3 +3,5 @@ import styled from 'styled-components'
 export const NextButton = styled.button`
   background: pink;
 `
+export const AnswerButton = styled.button`
+`
--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
-import { NextButton } from './Button'
+import { NextButton, AnswerButton } from './Button'
+
 
 export const CurrentQuestion = () => {
   const dispatch = useDispatch()
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
+  const answer = useSelector((state) => state.quiz.answers.find((a) => ( a.questionId === question.id // question could come from the previous selector in the last example
+    )))
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
   return (
     <div>
-      <h1>Question: {question.questionText}</h1>
+      <h1>Question:{question.id}/5 </h1>
+      <h2>{question.questionText}</h2>
       {/* Here we need to add the question options */}
       {/* Use map to display the answers. */}
       { question.options.map(
         (option, index) => 
-        <button 
+        <AnswerButton 
+        background={answer. ? 'green' : 'red'}
         key={index}
         onClick={(() => 
           dispatch(quiz.actions.submitAnswer(
@@ -26,8 +30,10 @@ export const CurrentQuestion = () => {
             questionId: question.id, 
             answerIndex: index
           }
-        )))}>{option}</button>
+        )))}>{option}
+        </AnswerButton>
       )}
+      
       <NextButton className='correct-answer' onClick={(() => 
       dispatch(quiz.actions.goToNextQuestion()))}>
         Next
